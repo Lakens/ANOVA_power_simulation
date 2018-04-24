cd16c2c6c22afb22563f35130864384203994c03
Daniel Lakens <Lakens@gmail.com>
2018-04-24 13:30:56 +0200

    working matrix assignment

--- tryout.R
+++ tryout.R
@@ -1,4 +1,3 @@
-
 options(scipen=999)
 
 library(mvtnorm)
@@ -10,100 +9,94 @@ library(tidyr)
 require(gridExtra)
 require(reshape2)
 
-
-mu = c(1, 0, 0, 0, 0, 0, 0, 0) # population means
-sd=1 #population standard deviations
-r=0.0 # correlation between repeated measures
-n<-1 #number of subjects
-p_adjust <- "none"
-
-string <- "4*2"
+#String used to specify the design
+# e.g., "2" for 1 factor, "2*2*2" for three factors
+string <- "2b*2w" #String used to specify the design
 factors <- length(as.numeric(strsplit(string, "\\D+")[[1]]))
+
+as.list(strsplit(string, ("[^A-Za-z]+")))
+
+design <- strsplit(gsub("[^A-Za-z]","",string),"",fixed=TRUE)[[1]]
+design <- rep(design, as.list(strsplit(string, "\\D+")[[1]]))
+design <- as.numeric(design == "w") #if within design, set value to 0, otherwise to 1
+
+design*design
+
+
+help("regmatches")
+if(factors == 1){frml1 <- as.formula("y ~ a + Error(subject/a)")}
+if(factors == 1){frml2 <- as.formula("~a")}
+if(factors == 2){frml1 <- as.formula("y ~ a*b + Error(subject/a*b)")}
+if(factors == 2){frml2 <- as.formula("~a+b")}
+if(factors == 3){frml1 <- as.formula("y ~ a*b*c + Error(subject/a*b*c)")}
+if(factors == 3){frml2 <- as.formula("~a+b+c")}
+if(factors == 4){frml1 <- as.formula("y ~ a*b*c*d + Error(subject/a*b*c*d)")}
+if(factors == 4){frml2 <- as.formula("~a+b+c+d")}
+
+#indicate which adjustment for multiple comparisons you want to use (e.g., "holm")
+p_adjust <- "none" 
+
+# specify means. 
+#for 1x2: c(1, 2) so 2 means
+#for 2x2: c(1, 2, 3, 4) so 4 means
+#for 2x2x2: c(1, 2, 3, 4, 5, 6, 7, 8) so 8 means
+mu = c(1, 2, 2, 2) # population means - should match up with the design
+sd=1 #population standard deviations
+r=0.5 # correlation between repeated measures
+n<-500 #number of subjects
+nsims = 100 # how many simulation replicates?
 
 #create matrix
 sigmatrix <- matrix(r, length(mu),length(mu)) #create a matrix filled with value of correlation, nrow and ncol set to length in mu
 diag(sigmatrix) <- sd # replace the diagonal with the sd
 
-a <- as.data.frame(rmvnorm(n=n,
-                           mean=mu,
-                           sigma=sigmatrix))
-
-a$subject<-as.factor(c(1:n))
-a <- melt(a, 
-          id.vars = "subject", 
-          variable.name = "cond",
-          value.name = "y")
-# a$X1 <- as.factor(c(rep("A1", nrow(a)/2), 
-#                     rep("A2", nrow(a)/2)))
-
-
 df <- as.data.frame(rmvnorm(n=n,
                             mean=mu,
                             sigma=sigmatrix))
-df$subject<-as.factor(c(1:n))
-df <- melt(df, 
-           id.vars = "subject", 
-           variable.name = "cond",
-           value.name = "y")
+str(df)
+colMeans(df)
 
-
-# prod(as.numeric(strsplit(string, "\\D+")[[1]]))/(as.numeric(strsplit(string, "\\D+")[[1]])[j])
-
-for(j in 1:factors){
-  # Let's break this down - it's a bit tricky. First, we want to create a list of a1 a2 b1 b2 that will indicate the factors. 
-  # We are looping this over the number of factors.
-  # This: as.numeric(strsplit(string, "\\D+")[[1]]) - takes the string used to specify the design and turn it in a list. 
-  # we take the letters from the alfabet: paste(letters[[j]] and add numbers 1 to however many factors there as: 1:as.numeric(strsplit(string, "\\D+")[[1]])[j], sep="")
-  # We this get e.g. ,a1 a2 - we repeat these each: n*(2^(factors-1)*2)/(2^j) and them times:  (2^j/2) to get a list for each factor
-  # We then bind these together with the existing dataframe.
-  df <- cbind(df, as.factor(unlist(rep(as.list(paste(letters[[j]], 
-                                                     1:as.numeric(strsplit(string, "\\D+")[[1]])[j], 
-                                                     sep="")), 
-                                       each = prod(as.numeric(strsplit(string, "\\D+")[[1]]))/prod(as.numeric(strsplit(string, "\\D+")[[1]])[1:j]),
-                                       times = prod(as.numeric(strsplit(string, "\\D+")[[1]]))/prod(as.numeric(strsplit(string, "\\D+")[[1]])[j:factors])
-  ))))
-}
-
-prod(as.numeric(strsplit(string, "\\D+")[[1]][1:2]))
-  
-  
-# First factor: each = prod*n/(factor level), times = 2^(j-1)
-# SEcond factor: each = prod*n/(factor level), times = 2
-
-
-names(df)[4:(3+factors)] <- letters[1:factors]
-# We perform the ANOVA using AFEX
-within.aov<-aov_car(frml1, #here we use frml1 to enter fromula 1 as designed above on the basis of the design 
-                    data=df,
-                    anova_table = list(es = "pes", p_adjust_method = p_adjust)) #This reports PES not GES
-# pairwise comparisons
-pc <- pairs(emmeans(within.aov, frml2), adjust = p_adjust) #no adjustments
-# store p-values and effect sizes for calculations and plots.
+cor(df)
 
 
 
 
 
-rep(as.list(paste(letters[[j]], 
-                  1:as.numeric(strsplit(string, "\\D+")[[1]])[j], 
-                  sep="")), 
-    each = n * (prod(as.numeric(strsplit(string, "\\D+")[[1]]))/(as.numeric(strsplit(string, "\\D+")[[1]])[1])^(j-1))/(as.numeric(strsplit(string, "\\D+")[[1]])[1]),
-    times = prod(as.numeric(strsplit(string, "\\D+")[[1]]))/prod(as.numeric(strsplit(string, "\\D+")[[1]])[j:length(as.numeric(strsplit(string, "\\D+")[[1]]))]))
+
+design
+sigma=matrix(c(sd,r,r,r,r,r,r,r,
+               r,sd,r,r,r,r,r,r,
+               r,r,sd,r,r,r,r,r,
+               r,r,r,sd,r,r,r,r,
+               r,r,r,r,sd,r,r,r,
+               r,r,r,r,r,sd,r,r,
+               r,r,r,r,r,r,sd,r,
+               r,r,r,r,r,r,r,sd), 8,8)
+
+sss <- matrix(NA, length(mu),length(mu))
+diag(sss) <- sd
+sss[1:2,1:2] <- matrix(design, 2,2)
 
 
-j=1
-string <- "2*3"
-rep(as.list(paste(letters[[j]], 
-                  1:as.numeric(strsplit(string, "\\D+")[[1]])[j], 
-                  sep="")), 
-    each = 3,
-    times = 1)
-
-j=1
-string <- "4*2*3"
-prod(as.numeric(strsplit(string, "\\D+")[[1]]))/prod(as.numeric(strsplit(string, "\\D+")[[1]])[1:j]) #each
-
-prod(as.numeric(strsplit(string, "\\D+")[[1]]))/prod(as.numeric(strsplit(string, "\\D+")[[1]])[j:length(as.numeric(strsplit(string, "\\D+")[[1]]))]) #times
 
 
-length(as.numeric(strsplit(string, "\\D+")[[1]]))
+
+nm[] <- mydata[3,3]
+
+
+
+#create new matrix
+mydata <- data.frame(Var1 = c("A", "A", "B"), Var2 = c("B", "C", "C"), values = c(2, 3, 6))
+vals<-sort(unique(c(as.character(mydata$Var1), as.character(mydata$Var2))))
+nm<-matrix(NA, nrow=length(vals), ncol=length(vals), dimnames=list(vals, vals))
+diag(nm)<-1
+
+#fill 
+nm[as.matrix(mydata[, 1:2])] <- mydata[,3]
+
+#symmetric
+nm[lower.tri(nm)] <- nm[upper.tri(nm)]
+nm
+
+
+
