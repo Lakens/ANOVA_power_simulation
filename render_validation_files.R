58a86d3b5046256114fff78b0108615009b564cf
Daniel Lakens <Lakens@gmail.com>
2019-03-19 10:52:27 +0100

    validation for 1x2 and 1x3 (between) and 2x1 and 3x1 done. Included R file to render all validation files.

--- render_validation_files.R
+++ render_validation_files.R
@@ -2,3 +2,10 @@ rmarkdown::render("validation_files/1.1_validation_power_between_1x2.Rmd")
 rmarkdown::render("validation_files/1.1_validation_power_between_1x2.md", output_format = "pdf_document")
 rmarkdown::render("validation_files/1.2_validation_power_between_1x3.Rmd")
 rmarkdown::render("validation_files/1.2_validation_power_between_1x3.md", output_format = "pdf_document")
+rmarkdown::render("validation_files/2.1_validation_power_within_2x1.Rmd")
+rmarkdown::render("validation_files/2.1_validation_power_within_2x1.md", output_format = "pdf_document")
+
+
+
+rmarkdown::render("validation_files/final_pdf/1.1_validation_power_between_1x2.md", output_format = "pdf_document")
+rmarkdown::render("validation_files/final_pdf/1.2_validation_power_between_1x3.md", output_format = "pdf_document")
