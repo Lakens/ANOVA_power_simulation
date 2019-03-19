8e74fdb634c5e26e0e116768ba8703011c8038f5
arcaldwell49 <arcaldwell49@gmail.com>
2019-03-19 13:44:01 -0500

    Updated Shiny app. Download pdf now functional again.

--- shiny_app/app.R
+++ shiny_app/app.R
@@ -977,7 +977,7 @@ server <- function(input, output) {
   #Create downloadable report in markdown TINYTEX NEEDS TO BE INSTALLED 
   output$report <- downloadHandler(
     # For PDF output, change this to "report.pdf"
-    filename = "report.html",
+    filename = "report.pdf",
     content = function(file) {
       # Copy the report file to a temporary directory before processing it, in
       # case we don't have write permissions to the current working dir (which
